87527ae404f050ee358c111ff50a5be80367af7b
UrbanLevente <urban.levente09+new_github@gmail.com>
2025-05-12 23:31:18 +0200

    Addedd libraries and removed console for release builds

--- Editor/Main.cpp
+++ Editor/Main.cpp
@@ -1,6 +1,76 @@
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+
+#ifdef _WIN32
+#ifndef DEBUG
+#include <Windows.h>
+#endif
+#endif
+
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-int main() {
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
 
-    return 0;
+int app_main();
+
+#ifdef DEBUG
+
+int main()
+{
+	return app_main();
+}
+
+#else
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	return app_main();
+}
+
+#endif
+
+int app_main()
+{
+	if (!glfwInit())
+	{
+		std::cerr << "Failed to initialize GLFW\n";
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Chernobyl Simulator Editor", nullptr, nullptr);
+	if (!window)
+	{
+		std::cerr << "Failed to create GLFW window\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cerr << "Failed to initialize GLAD\n";
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
---
+++ Engine/Core/Engine.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "EngineAPI.h"
+
+ENGINE_API void InitEngine();
